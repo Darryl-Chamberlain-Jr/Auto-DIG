71158b82309a8cecd29d282a27713cd76478005e
Darryl Chamberlain <dchamberlain31@gmail.com>
2021-02-24 09:27:58 -0500

    Removed unnecessary imports from Module 1

--- Code/Core/01realComplex/orderOfOperationsCopy.py
+++ Code/Core/01realComplex/orderOfOperationsCopy.py
@@ -1,13 +1,5 @@
 import sys
-from sympy import *
-import numpy
 import random
-import math
-from decimal import Decimal
-import decimal
-import traceback
-import cmath
-import matplotlib.pyplot as plt
 
 DIR=sys.argv[1]
 debug=sys.argv[2]
