68c76feab081dbbe10ffd3cf2438bb99b5007408
Darryl Chamberlain <dchamberlain31@gmail.com>
2021-03-04 16:24:56 -0500

    Created MA_8_F_1_2 equation and graph. Questions are for CRIF project and ask students to identify whether the presented equation/graph is a linear function.

---
+++ Code/CRIF/MA_8_F_1_2_graph.py
@@ -0,0 +1,213 @@
+### NECESSARY IMPORTS ###
+import sys
+import numpy
+import random
+import matplotlib.pyplot as plt
+
+### System variables ###
+DIR=sys.argv[1]
+debug=sys.argv[2]
+if debug == "save":
+    database_name=sys.argv[3]
+    question_list=sys.argv[4]
+    version=sys.argv[5]
+    thisQuestion=sys.argv[6]
+    OS_type=sys.argv[7]
+else:
+    version="Z"
+    thisQuestion="debug_image"
+sys.path.insert(1, f"/{DIR}/PythonScripts/ScriptsForQuestionCode")
+from commonlyUsedFunctions import *
+from intervalMaskingMethod import *
+sys.path.insert(1, f"/{DIR}/PythonScripts/ScriptsForDatabases")
+from storeQuestionData import *
+
+### DEFINITIONS ###
+# This should list all definitions used. Be sure to check the commonly used functions file before creating a new one.
+def graph_polynomial_function(degree):
+    a = (-1)**random.randint(0,1) * random.randint(1, 5)
+    h = (-1)**random.randint(0,1) * random.randint(1, 5)
+    k = (-1)**random.randint(0,1) * random.randint(1, 5)
+    if degree == 1:
+        if k > 0:
+            equation = f"{a}({generatePolynomialDisplay([1, -h])})+{k}"
+        else:
+            equation = f"{a}({generatePolynomialDisplay([1, -h])})-{-k}"
+    else:
+        if k > 0:
+            equation = f"{a}({generatePolynomialDisplay([1, -h])})^{degree}+{k}"
+        else:
+            equation = f"{a}({generatePolynomialDisplay([1, -h])})^{degree}-{-k}"
+    SMALL_SIZE = 24
+    MEDIUM_SIZE = 28
+    BIGGER_SIZE = 32
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    plt.figure(1)
+    graphX = numpy.arange(h-3, h+3, 0.01)
+    graphY = a*(graphX-h)**degree+k
+    plt.plot(graphX, graphY, linewidth = 5, color = '#02325f')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.savefig('/' + str(DIR) + '/Figures/' + str(thisQuestion) + str(version) + '.png', bbox_inches='tight')
+    plt.close()
+    return equation
+def cubeRootThis(value):
+    if value > 0:
+        return (value)**(1./3.)
+    else:
+        return -(-value)**(1./3.)
+def graph_rational_function():
+    rootDegree = random.randint(2, 3)
+    coefficient = (-1)**random.randint(0, 1) * random.randint(1, 4)
+    h1 = maybeMakeNegative(random.randint(3, 7))
+    h2 = maybeMakeNegative(random.randint(3, 7))
+    k = maybeMakeNegative(random.randint(3, 7))
+    fixed_point = [float(h2/h1), k]
+    if rootDegree == 2:
+        if h1 > 0:
+            xPlot = numpy.arange(fixed_point[0], fixed_point[1]+4, 0.05)
+        else:
+            xPlot = numpy.arange(fixed_point[0]-4, fixed_point[1], 0.05)
+        yPlot = [coefficient*(h1*xPlot[i]-h2)**0.5+k for i in range(len(xPlot))]
+        if k > 0:
+            equation = "{%d}\\sqrt{%s}+%d" %(coefficient, generatePolynomialDisplay([h1, -h2]), k)
+        else:
+            equation = "{%d}\\sqrt{%s}-%d" %(coefficient, generatePolynomialDisplay([h1, -h2]), -k)
+    else:
+        xPlot = numpy.arange(fixed_point[0]-3, fixed_point[0]+3, 0.05)
+        yPlot = [coefficient*cubeRootThis(h1*xPlot[i]-h2)+k for i in range(len(xPlot))]
+        if k > 0:
+            equation = "{%d}\\sqrt[3]{%s}+%d" %(coefficient, generatePolynomialDisplay([h1, -h2]), k)
+        else:
+            equation = "{%d}\\sqrt[3]{%s}-%d" %(coefficient, generatePolynomialDisplay([h1, -h2]), -k)
+    SMALL_SIZE = 24
+    MEDIUM_SIZE = 28
+    BIGGER_SIZE = 32
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    plt.figure(1)
+    plt.plot(xPlot, yPlot, linewidth = 5, color = '#02325f')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.savefig('/' + str(DIR) + '/Figures/' + str(thisQuestion) + str(version) + '.png', bbox_inches='tight')
+    plt.close()
+    return equation
+def graph_absolute_value_function():
+    a = (-1)**random.randint(0,1) * random.randint(1, 5)
+    h = (-1)**random.randint(0,1) * random.randint(1, 5)
+    k = (-1)**random.randint(0,1) * random.randint(1, 5)
+    if k > 0:
+        equation = f"{a}|{generatePolynomialDisplay([1, -h])}|+{k}"
+    else:
+        equation = f"{a}|{generatePolynomialDisplay([1, -h])}|-{-k}"
+    SMALL_SIZE = 24
+    MEDIUM_SIZE = 28
+    BIGGER_SIZE = 32
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    plt.figure(1)
+    graphX = numpy.arange(h-3, h+3, 0.01)
+    graphY = a*abs(graphX-h)+k
+    plt.plot(graphX, graphY, linewidth = 5, color = '#02325f')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.savefig('/' + str(DIR) + '/Figures/' + str(thisQuestion) + str(version) + '.png', bbox_inches='tight')
+    plt.close()
+    return equation
+def graph_exponential_function():
+    a = (-1)**random.randint(0,1) * random.randint(1, 5)
+    h = (-1)**random.randint(0,1) * random.randint(1, 5)
+    k = (-1)**random.randint(0,1) * random.randint(1, 5)
+    base = random.choice([0.5, 2])
+    if base == 0.5:
+        display_base = "\\left( \\dfrac{1}{2} \\right)"
+    else:
+        display_base = "(2)"
+    if k > 0:
+        equation = "%d %s^{x-%d}+%d" %(a, display_base, h, k)
+    else:
+        equation = "%d %s^{x-%d}-%d" %(a, display_base, h, -k)
+    SMALL_SIZE = 24
+    MEDIUM_SIZE = 28
+    BIGGER_SIZE = 32
+    plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+    plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+    plt.rc('axes', labelsize=MEDIUM_SIZE)    # fontsize of the x and y labels
+    plt.rc('xtick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
+    plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
+    plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+    plt.figure(1)
+    graphX = numpy.arange(h-3, h+3, 0.01)
+    graphY = a*(base)**(graphX-h)+k
+    plt.plot(graphX, graphY, linewidth = 5, color = '#02325f')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.grid(True)
+    plt.savefig('/' + str(DIR) + '/Figures/' + str(thisQuestion) + str(version) + '.png', bbox_inches='tight')
+    plt.close()
+    return equation
+
+### VARIABLE DECLARATIONS ###
+# Declare the necessary variables.
+graph_is_linear = random.randint(0, 1)
+if graph_is_linear == 0:
+    solution="no, the graph is not linear."
+    answerLetter = "B"
+    yes_comment="A linear function has a constant rate of growth. As $x$ increases/decreases, $y$ increases/decreases at the same rate. The graph in this example does not have a constant rate of change."
+    no_comment="* Correct! The graph does not have a constant rate of change and thus is not a linear function."
+    non_linear_choice = random.choice(["polynomial", "rational", "absolute_value", "exponential"])
+    if non_linear_choice == "polynomial":
+        degree = random.randint(2, 5)
+        equation=graph_polynomial_function(degree)
+    elif non_linear_choice == "rational":
+        equation=graph_rational_function()
+    elif non_linear_choice == "absolute_value":
+        equation=graph_absolute_value_function()
+    else:
+        equation=graph_exponential_function()
+else:
+    solution="yes, the graph is linear."
+    answerLetter = "A"
+    equation=graph_polynomial_function(1)
+    yes_comment="* Correct! The graph has a constant rate of change and is thus a linear function."
+    no_comment="A linear function has a constant rate of growth. As $x$ increases/decreases, $y$ increases/decreases at the same rate. The graph in this example does have a constant rate of change."
+
+displayStem = "Is the graph below a linear function?"
+displayProblem = f"{thisQuestion}{version}"
+displaySolution = solution
+
+choices=["Yes, the graph is linear", "No, the graph is not linear."]
+choiceComments=[f"{yes_comment}", f"{no_comment}"]
+generalComment=f"The equation graphed was {equation}. A linear function has a constant rate of growth. This means that as $x$ increases or decreases, $y$ increase or decreases at the same rate. For example, $x^2$ is NOT a linear function. As $x$ increases, the $y$ increases faster and faster. From $x=1$ to $x=2$, the $y$ increases by 3. From $x=2$ to $x=3$, the $y$ increases by 5. From $x=3$ to $x=4$, the $y$ increases by 7. A linear function would have the same change in $y$ for any change in $x$."
+
+### Define display variables ###
+# Options are: "String", "Math Mode", "Graph", or "Table"
+displayStemType="String"
+displayProblemType="Graph"
+displayOptionsType="String"
+
+### Writes important information to database. ###
+if debug=="save":
+    writeToDatabase(OS_type, DIR, database_name, question_list, thisQuestion, displayStemType, displayStem, displayProblemType, displayProblem, displayOptionsType, choices, choiceComments, displaySolution, answerLetter, generalComment)
+else:
+    print_for_debugger(displayStemType, displayStem, displayProblemType, displayProblem, displayOptionsType, choices, choiceComments, displaySolution, answerLetter, generalComment)
---
+++ Code/CRIF/MA_8_F_1_2_table.py
@@ -0,0 +1,70 @@
+### NECESSARY IMPORTS ###
+import sys
+from sympy import *
+import numpy
+import random
+import math
+from decimal import Decimal
+import decimal
+import traceback
+import cmath
+import matplotlib.pyplot as plt
+from sympy.abc import x, y
+from sympy.solvers import solve
+
+### System variables ###
+DIR=sys.argv[1]
+debug=sys.argv[2]
+if debug == "save":
+    database_name=sys.argv[3]
+    question_list=sys.argv[4]
+    version=sys.argv[5]
+    thisQuestion=sys.argv[6]
+    OS_type=sys.argv[7]
+else:
+    version="Z"
+    thisQuestion="debug_image"
+sys.path.insert(1, f"/{DIR}/PythonScripts/ScriptsForQuestionCode")
+from commonlyUsedFunctions import *
+from intervalMaskingMethod import *
+sys.path.insert(1, f"/{DIR}/PythonScripts/ScriptsForDatabases")
+from storeQuestionData import *
+
+### DEFINITIONS ###
+# This should list all definitions used. Be sure to check the commonly used functions file before creating a new one.
+
+### VARIABLE DECLARATIONS ###
+# Declare the necessary variables.
+
+### CREATE INTERVAL OPTIONS
+# Use the General Method Function 'createIntervalOptions(setOfValues, intervalRange, precision)'.
+    # *setOfValues* is the set of values you are trying to disguise in intervals. This can be a set of sets!
+    # setOfValues = [set1, set2, set3, set4]
+        # Be sure all sets are of equal length. This is necessary when checking that values are not contained in other intervals.
+    # *intervalRange* is the approximate size of the interval masking the values. This should be small when values are close to each other and larger when values are further away. In the future, this may be automatically determined using a min distance between values.
+    # *precision* is an optional input. If specified, it is used to fine-tune the endpoint of the intervals used to mask values. Should only be included when values are extremely close together.
+
+### USE INTERVAL OPTIONS OUTPUT TO CREATE ANSWERLIST
+# answerList should be created as a list of sets, where each set is [choice, comment, answerLetterIndicator].
+    # *choice* is exactly the way the option should be displayed. This should include math mode or display of intervals.
+    # *comment* is the related comment that should be displayed on the key. It should include the exact answer (if the value is being disguised in an interval) and start with a * if it is the solution.
+    # *answerLetterIndicator* is 1 for the correct option and 0 otherwise. This is used to note the correct letter for the question.
+    # answerList = [solutionSet, distractor1Set, distractor2Set, distractor3Set, distractor4Set]
+
+### DEFINE CHOICES, CHOICE COMMENTS, AND ANSWER LETTER
+    # choices = [answerList[0][0], answerList[1][0], answerList[2][0], answerList[3][0], answerList[4][0]
+    # choiceComments = [answerList[0][1], answerList[1][1], answerList[2][1], answerList[3][1], answerList[4][1]]
+    # answerLetterIndicators = [answerList[0][2], answerList[1][2], answerList[2][2], answerList[3][2], answerList[4][2]]
+    # answerLetter = identifyAnswerLetter(answerLetterIndicators)
+
+### Define display variables ###
+# Options are: "String", "Math Mode", "Graph", or "Table"
+displayStemType="String"
+displayProblemType="Math Mode"
+displayOptionsType="Math Mode"
+
+### Writes important information to database. ###
+if debug=="save":
+    writeToDatabase(OS_type, DIR, database_name, question_list, thisQuestion, displayStemType, displayStem, displayProblemType, displayProblem, displayOptionsType, choices, choiceComments, displaySolution, answerLetter, generalComment)
+else:
+    print_for_debugger(displayStemType, displayStem, displayProblemType, displayProblem, displayOptionsType, choices, choiceComments, displaySolution, answerLetter, generalComment)
