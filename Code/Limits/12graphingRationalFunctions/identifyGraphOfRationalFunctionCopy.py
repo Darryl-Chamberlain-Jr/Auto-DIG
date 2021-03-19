323cb17bd5d568bb0e9735b5f2b29c5256e20b3e
Darryl Chamberlain <dchamberlain31@gmail.com>
2021-03-18 20:51:38 -0400

    Revised M12L Identify Graph of Rational Function.

--- Code/Limits/12graphingRationalFunctions/identifyGraphOfRationalFunctionCopy.py
+++ Code/Limits/12graphingRationalFunctions/identifyGraphOfRationalFunctionCopy.py
@@ -1,15 +1,7 @@
 import sys
-from sympy import *
 import numpy
 import random
-import math
-from decimal import Decimal
-import decimal
-import traceback
-import cmath
 import matplotlib.pyplot as plt
-from sympy.abc import x, y
-from sympy.solvers import solve
 
 DIR=sys.argv[1]
 debug=sys.argv[2]
@@ -29,23 +21,29 @@ sys.path.insert(1, f"/{DIR}/PythonScripts/ScriptsForDatabases")
 from storeQuestionData import *
 
 def coefficientsForPoly(zeros):
-    a, b, c = zeros  #(x-a)(x-b)(x-c)
-    return [1, -c-a-b, a*b+a*c+b*c, -a*b*c]
+    polynomial = numpy.poly1d(zeros, True)
+    float_coefficients = polynomial.c
+    integer_coefficients = []
+    for i in float_coefficients:
+        integer_coefficients.append(i)
+    return integer_coefficients
+
 def graph_rational_function_with_undefined_points(numerator_zeros, denominator_zeros, step_size):
+    undefined_values=[]
     vertical_asymptotes=[]
     holes=[]
-    for denom_zero in denominator_zeros:
-        if denom_zero in numerator_zeros:
+    list_numerator_zeros=[]
+    list_denominator_zeros=[]
+    for numerator_zero in numerator_zeros:
+        list_numerator_zeros.append(numerator_zero[1]/numerator_zero[0])
+    for denominator_zero in denominator_zeros:
+        list_denominator_zeros.append(denominator_zero[1]/denominator_zero[0])
+        undefined_values.append(denominator_zero[1]/denominator_zero[0])
+    for denom_zero in list_denominator_zeros:
+        if denom_zero in list_numerator_zeros:
             holes.append(denom_zero)
         else:
             vertical_asymptotes.append(denom_zero)
-    list_of_zeros=[]
-    undefined_values=[]
-    for numerator_zero in numerator_zeros:
-        list_of_zeros.append(numerator_zero[1]/numerator_zero[0])
-    for denominator_zero in denominator_zeros:
-        list_of_zeros.append(denominator_zero[1]/denominator_zero[0])
-        undefined_values.append(denominator_zero[1]/denominator_zero[0])
     undefined_values=sorted(undefined_values)
     x_values=[]
     index=0
@@ -63,10 +61,25 @@ def graph_rational_function_with_undefined_points(numerator_zeros, denominator_z
     for x_value in x_values:
         temp_y=( (numerator_zeros[0][0]*x_value - numerator_zeros[0][1])*(numerator_zeros[1][0]*x_value - numerator_zeros[1][1])*(numerator_zeros[2][0]*x_value - numerator_zeros[2][1]) ) / ( (denominator_zeros[0][0]*x_value - denominator_zeros[0][1])*(denominator_zeros[1][0]*x_value - denominator_zeros[1][1])*(denominator_zeros[2][0]*x_value - denominator_zeros[2][1]) )
         y_values.append(temp_y)
-    plt.plot(x_values, y_values, linewidth=5, color='blue')
-    #plt.ylim([float(-10),float(10)])
+    plt.plot(x_values, y_values, linewidth=2, color='blue')
+    numerator=numpy.poly1d(list_numerator_zeros, True)
+    denominator=numpy.poly1d(list_denominator_zeros, True)
     for va in vertical_asymptotes:
-        plt.axvline(x= va[1], ls=('dashed'), color='black')
+        plt.axvline(x= va, ls=('dashed'), color='black')
+        plt.text(va, numerator(va-0.01)/denominator(va-0.01), f"x={va}", fontsize=18, horizontalalignment='right')
+    flip_text_placement=1
+    for hole in holes:
+        num_value=int(numerator(hole))
+        denom_value=int(denominator(hole))
+        while num_value==0 and denom_value==0:
+            numerator=numpy.polyder(numerator)
+            denominator=numpy.polyder(denominator)
+            num_value=int(numerator(hole))
+            denom_value=int(denominator(hole))
+        hole_y_value=float(num_value/denom_value)
+        plt.plot(hole, hole_y_value, 'o', color='black', markersize=10, mfc='none')
+        plt.text(hole, flip_text_placement*3*hole_y_value, f"Hole at {int(hole)}", fontsize=18)
+        flip_text_placement=flip_text_placement*-1
     #for hole in holes:
     #    plt.scatter(hole[1], y_hole, s=80, facecolors='none', edgecolors='r')
     plt.grid(True)
@@ -92,7 +105,7 @@ else:
 vertical_asymptotes=list(numpy.setdiff1d(denominator_zeros, holes) )
 
 # Solution
-step_size=0.05
+step_size=0.025
 graph_rational_function_with_undefined_points( [  [1, numerator_zeros[0]],   [1, numerator_zeros[1]],  [1, numerator_zeros[2]]  ],  [  [1, denominator_zeros[0]], [1, denominator_zeros[1]],  [1, denominator_zeros[2]] ], step_size)
 num_poly_solution=generatePolynomialDisplay(coefficientsForPoly(numerator_zeros))
 denom_poly_solution=generatePolynomialDisplay(coefficientsForPoly(denominator_zeros))
@@ -119,7 +132,7 @@ distractor_3_numerator_zeros=list(numpy.concatenate( ([i*-1 for i in other_numer
 distractor_3_denominator_zeros=[i*-1 for i in denominator_zeros]
 num_poly_distractor_3=generatePolynomialDisplay(coefficientsForPoly(distractor_3_numerator_zeros))
 denom_poly_distractor_3=generatePolynomialDisplay(coefficientsForPoly(distractor_3_denominator_zeros))
-display_distractor_3="f(x)=\\frac{%s}{%s}" %(num_poly_distractor_2, denom_poly_distractor_3)
+display_distractor_3="f(x)=\\frac{%s}{%s}" %(num_poly_distractor_3, denom_poly_distractor_3)
 
 # Distractor 4 - None of the above
 
